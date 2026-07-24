b7acc31012a409fdff2d0d4873349f7c0bca87ca
srava <srava@sravkirru>
2026-07-24 11:19:49 +0530

    variables&datatypes

---
+++ CoreJava/src/com/coders/datatypeprogram/Datatype.java
@@ -0,0 +1,25 @@
+package com.coders.datatypeprogram;
+
+public class Datatype {
+	public static void main(String[] args) {
+		int a = 4;
+		char b = 'S';
+		String name = "Sravani";
+		float c = 83.8f;
+		double d = 45668.456;
+		short e = 65;
+		boolean f =true;
+		long g = 1234567890L;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(name);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+		
+	}
+
+}
---
+++ CoreJava/src/com/coders/variableprogram/Fourth.java
@@ -0,0 +1,13 @@
+package com.coders.variableprogram;
+
+public class Fourth {
+	public static void main(String[] args) {
+		int age = 21;
+		age = 22;
+		String name = "Sravani";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(name + " " + age);
+	}
+
+}
