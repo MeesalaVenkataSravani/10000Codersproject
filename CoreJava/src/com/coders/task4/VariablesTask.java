c121585311cbdcf21163adf51b24a6ca16d26418
srava <srava@sravkirru>
2026-07-27 16:45:37 +0530

    Tasks

---
+++ CoreJava/src/com/coders/task4/VariablesTask.java
@@ -0,0 +1,25 @@
+package com.coders.task4;
+
+public class VariablesTask {
+
+	public static void main(String[] args) {
+		byte month = 12;
+		short year = 2026;
+		int age = 21;
+		long phnnumber = 6304607559L;
+		float height = 4.8f;
+		double salary = 25000;
+		char letter = 'S';
+		boolean set = false;
+		System.out.println(month);
+		System.out.println(year);
+		System.out.println(age);
+		System.out.println(phnnumber);
+		System.out.println(height);
+		System.out.println(salary);
+		System.out.println(letter);
+		System.out.println(set);
+
+	}
+
+}
