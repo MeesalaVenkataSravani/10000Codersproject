51759859432c49648a404172217563b1ab00b534
srava <srava@sravkirru>
2026-07-28 11:24:39 +0530

    28/7/26

---
+++ CoreJava/src/com/coders/decisionstatements/DecisionMakingStatements.java
@@ -0,0 +1,11 @@
+package com.coders.decisionstatements;
+
+public class DecisionMakingStatements {
+	public static void main(String[] args) {
+		int a=9;
+		if (a>0) {
+			System.out.println("Num is a Positive");
+		}
+	}
+
+}
