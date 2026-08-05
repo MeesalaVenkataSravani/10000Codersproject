e0aa1c0f8bbd3d3161423a5bc2422ca7a50c6738
srava <srava@sravkirru>
2026-08-05 16:37:56 +0530

    Patterns

---
+++ CoreJava/src/com/coders/patternstask/SquareStar.java
@@ -0,0 +1,17 @@
+package com.coders.patternstask;
+
+public class SquareStar {
+
+	public static void main(String[] args) {
+		int m = 5;
+		int n = 5;
+		for (int i = 1;i<=m;i++) {
+			for (int j=1;j<=n;j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
